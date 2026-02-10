64cf0bfdb03564c6b9200725b1a1949df0d26f7a
Joe Vaughan <you@example.comjosephvaughan2000@gmail.com>
2026-02-10 10:39:27 +0000

    adding capacitor packages externally to avoid SSR issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  external: [
-    'react',
-    'react-dom',
-    'next',
-    'next/dynamic',
-    'next/image',
-    'next/link',
-    'next/router',
-  ],
-  treeshake: true,
-});
+  export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    external: [
+      'react',
+      'react-dom',
+      'next',
+      'next/dynamic',
+      'next/image',
+      'next/link',
+      'next/router',
+      // Capacitor packages - must be external to avoid SSR issues
+      /^@capacitor\/.*/,
+      /^@capacitor-community\/.*/,
+      /^@capgo\/capacitor.*/,
+    ],
+    treeshake: true,
+  });
